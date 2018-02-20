3fc1e63f339f0698de51fba88dd87ebcf04b2a25
Sebastian Rollen <rollenseb@gmail.com>
2018-02-20 18:38:50 +0000

    Converted initial_weights to Series

--- BCS.py
+++ BCS.py
@@ -16,28 +16,27 @@ if __name__ == '__main__':
     STARTING_CASH = 100000
     ALLOWED_DRIFT = 0.05
     MINKOWSKI_P = 2
-    relative_path = 'C:/Users/Tiger/PycharmProjects/BettermentCaseStudy'
+    RELATIVE_PATH = 'C:/Users/Tiger/PycharmProjects/BettermentCaseStudy'
 
     returns_df = pd.read_csv(
-        filepath_or_buffer=relative_path + '/portfolio_returns.csv',
+        filepath_or_buffer='{0}/portfolio_returns.csv'.format(RELATIVE_PATH),
         index_col=0,
         parse_dates=True
     )
     tickers = returns_df.columns
     dates = returns_df.index
-    initial_weights = pd.DataFrame(
-        data=dict(zip(tickers, [0.25, 0.25, 0.125, 0.125, 0.04, 0.035, 0.125, 0.05])),
-        index=[dates[0]]
+    target_weights = pd.Series(
+        data=[0.25, 0.25, 0.125, 0.125, 0.04, 0.035, 0.125, 0.05],
+        index=tickers
     )
-    print(initial_weights)
     minkowski = partial(
         minkowski_distance,
-        arr_2=initial_weights.values[0],
+        arr_2=target_weights.values,
         p=MINKOWSKI_P)
 
     # NON REBALANCED PORTFOLIO
     cum_returns_df = (returns_df+1).cumprod()
-    buy_and_hold_df = (cum_returns_df * STARTING_CASH) * initial_weights.values[0]
+    buy_and_hold_df = (cum_returns_df * STARTING_CASH).mul(target_weights, axis=1)
     buy_and_hold_df_alloc = (buy_and_hold_df.div(buy_and_hold_df.sum(axis=1), axis=0))
     buy_and_hold_df_returns = buy_and_hold_df.sum(axis=1).pct_change(1)
 
@@ -47,7 +46,7 @@ if __name__ == '__main__':
 
     for date in dates[1:]:
         if minkowski(rebalance_df_alloc.loc[date]) > ALLOWED_DRIFT:
-            rebalance_df.loc[date] = sum(rebalance_df.shift(1).loc[date] * (1 + returns_df.loc[date])) * initial_weights.values[0]
+            rebalance_df.loc[date] = sum(rebalance_df.shift(1).loc[date] * (1 + returns_df.loc[date])) * target_weights
         else:
             rebalance_df.loc[date] = rebalance_df.shift(1).loc[date] * (1 + returns_df.loc[date])
         rebalance_df_alloc.loc[date] = (rebalance_df.loc[date].div(rebalance_df.loc[date].sum(), axis=0))
